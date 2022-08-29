6020a96e8ad7f1895023fbd16ed3e37e92d1deb1
Franco <franco@Ariels-MacBook-Air.local>
2022-08-29 14:56:46 -0300

    Maquetacion de figma

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*.html"],
+  theme: {
+    colors:{
+      'softPink': '#f6ebda'
+    },
+    container:{
+      center:true
+    },
+    extend: {},
+  },
+  plugins: [],
+}
