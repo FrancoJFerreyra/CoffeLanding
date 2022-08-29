cf0dfeec95880118b814c567bfbd239cd5d680d5
Franco Ferreyra <ferreyra_franco02@outlook.com>
2022-08-29 19:18:18 -0300

    Styling navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,13 +2,30 @@
 module.exports = {
   content: ["*.html"],
   theme: {
+    fontFamily:{
+      'body':['"Poppins"', 'sans-serif']
+    },
+    fontSize:{
+      'menu__item': '1.063rem'
+    },
     colors:{
-      'softPink': '#f6ebda'
+      'softPink':'#f6ebda',
+      'orange':'#ff902b'
     },
     container:{
       center:true
     },
-    extend: {},
+    width:{
+        'nav':'72.563rem',
+        'nav__menu': '20.125rem'
+      },
+      height: {
+        'nav': '2.5rem',
+        'nav__menu': '1.688rem'
+      },
+    extend: {
+      
+    },
   },
   plugins: [],
 }
