1b59f7e98df5c8e2c57c6eeb48a86001dd4df538
Franco <franco@Ariels-Air.fibertel.com.ar>
2022-09-01 14:59:28 -0300

    Finalizando Pagina

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,12 +13,26 @@ module.exports = {
       'gray':'#7e7d7a',
       'white':'#ffffff',
       'yellow':'#f4ae26',
-      'black':'#000000'
+      'black':'#000000',
+      'opinionBorderCard':'rgba(255, 211, 144, 0.62)',
+      'opinionP':'#ffcb7c'
     },
     container:{
       center:true
     },
     extend: {
+      boxShadow:{
+        'popCard': '0 10px 10px 10px rgba(0,0,0, 0.25)',
+        'specialCard': '0 0 6px 0 rgba(0,0,0, 0.16)' 
+      },
+      backgroundImage:{
+        'about':"url('/assets/bgAboutUs.png')",
+        'opinion':"url('/assets/bgOpinions.png')",
+        'footerImg':"url('/assets/footerImg.jpeg')"
+      },
+      backgroundSize:{
+        '70%':'70%'
+      }
     },
   },
   plugins: [],
