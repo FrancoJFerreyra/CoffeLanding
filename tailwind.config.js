52a0bccff3b26fe304e413a35d2577561f585d3a
Franco <franco@Ariels-Air.fibertel.com.ar>
2022-09-07 15:27:41 -0300

    Cambiando a grillas

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,49 +1,50 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["*.html"],
+  content: ['*.html'],
+  important: true,
   theme: {
-    fontFamily:{
-      'body':['"Poppins"', 'sans-serif']
+    fontFamily: {
+      body: ['"Poppins"', 'sans-serif'],
     },
-    colors:{
-      'inherit':'inherit',
-      'softPink':'#f6ebda',
-      'orange':'#ff902b',
-      'softOrange':'f9d9aa',
-      'brown':'#2f2105',
-      'gray':'#7e7d7a',
-      'white':'#ffffff',
-      'yellow':'#f4ae26',
-      'black':'#000000',
-      'semiBlack':'#2f2105',
-      'opinionBorderCard':'rgba(255, 211, 144, 0.62)',
-      'opinionP':'#ffcb7c'
+    colors: {
+      inherit: 'inherit',
+      softPink: '#f6ebda',
+      orange: '#ff902b',
+      softOrange: 'f9d9aa',
+      brown: '#2f2105',
+      gray: '#7e7d7a',
+      white: '#ffffff',
+      yellow: '#f4ae26',
+      black: '#000000',
+      semiBlack: '#2f2105',
+      opinionBorderCard: 'rgba(255, 211, 144, 0.62)',
+      opinionP: '#ffcb7c',
     },
-    container:{
-      center:true
-    },screens:{
-        'xsm': '320px',
-        'sm': '640px',
-        'md': '768px',
-        'lg': '1024px',
-        'xl': '1280px',
-        '2xl': '1536px',
-      },
+    container: {
+      center: true,
+    },
+    screens: {
+      xsm: '320px',
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px',
+      '2xl': '1536px',
+    },
     extend: {
-      boxShadow:{
-        'popCard': '0 10px 10px 10px rgba(0,0,0, 0.25)',
-        'specialCard': '0 0 6px 0 rgba(0,0,0, 0.16)' 
+      boxShadow: {
+        card: '0 10px 10px 10px rgba(0,0,0, 0.25)',
+        specialCard: '0 0 6px 0 rgba(0,0,0, 0.16)',
       },
-      backgroundImage:{
-        'about':"url('/assets/bgAboutUs.png')",
-        'opinion':"url('/assets/bgOpinions.png')",
-        'footerImg':"url('/assets/footerImg.jpeg')"
+      backgroundImage: {
+        about: "url('/assets/bgAboutUs.png')",
+        opinion: "url('/assets/bgOpinions.png')",
+        footerImg: "url('/assets/footerImg.jpeg')",
       },
-      backgroundSize:{
-        '70%':'70%'
+      backgroundSize: {
+        '70%': '70%',
       },
-      
     },
   },
   plugins: [],
-}
+};
